53de41f34e46f3adbcd3c2d360ed8ec1e8984121
jenushanpremkumar <jenushan349@gmail.com>
2026-07-19 18:30:41 -0400

    feat: add multi-port scanning

--- scanner.py
+++ scanner.py
@@ -8,3 +8,22 @@ def scan_port(host, port):
     tcp_socket.settimeout(1)
 
     result = tcp_socket.connect_ex((host, port))
+
+    tcp_socket.close()
+
+    if result == 0: 
+        return True 
+    else:
+        return False
+    
+def scan_ports(host, ports):
+    print(f"Scanning {host}...\n")
+    for port in ports: 
+        if (scan_port(host, port)):
+            print(f"{port}: Open")
+        else: 
+            print(f"{port}: Closed")
+
+ports = [22, 80, 443, 8000, 9999]
+
+scan_ports("127.0.0.1", ports)
